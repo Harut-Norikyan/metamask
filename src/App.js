a998f91d695314d63f7f81c2286ccd0445c94999
Harut <harutnoro@mail.ru>
2023-05-11 00:46:30 +0400

    integration metamask wallet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Web3 from 'web3';
 
 function App() {
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+  const [recipient, setRecipient] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const handleConnectMetamask = async () => {
+    if (!window.ethereum) {
+      alert('Please install Metamask to connect to Ethereum network. Download Metamask at https://metamask.io/download/');
+      return;
+    }
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const web3 = new Web3(window.ethereum);
+      setWeb3(web3);
+      const accounts = await web3.eth.getAccounts();
+      setAccounts(accounts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDisconnectMetamask = () => {
+    setWeb3(null);
+    setAccounts([]);
+    console.log('Disconnected from Metamask!');
+  };
+
+  const handleSendTransaction = async () => {
+    if (web3) {
+      try {
+        const tx = {
+          from: accounts[0],
+          to: recipient,
+          value: web3.utils.toWei(amount.toString(), 'ether'),
+        };
+        const result = await web3.eth.sendTransaction(tx);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {web3 ? (
+        <div>
+          <p>Connected to Metamask</p>
+          <p>Accounts: {accounts.join(', ')}</p>
+          <button onClick={handleDisconnectMetamask}>Disconnect Metamask</button>
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Recipient Address"
+            value={recipient}
+            onChange={(event) => setRecipient(event.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Amount"
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+          />
+          <br />
+          <br />
+          <button onClick={handleSendTransaction}>Send Transaction</button>
+        </div>
+      ) : (
+        <div>
+          <p>Not connected to Metamask</p>
+          <button onClick={handleConnectMetamask}>Connect Metamask</button>
+        </div>
+      )}
     </div>
   );
 }
